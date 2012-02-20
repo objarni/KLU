26ec588b74d3abcac9ea84e5bfc4892d21a7f56f
objarni <olof.bjarnason@gmail.com>
2012-02-21 00:30:14 +0100

    Developers now have names. Another unit test added. Model skeleton added.

---
+++ model.py
@@ -0,0 +1,12 @@
+﻿class Model:
+    def __init__(self, units, ask_threshold):
+        pass
+    
+    def add_developer(self, name, knowledge):
+        pass
+        
+    def step(self, steps):
+        pass
+        
+    def get_knowledge(self, developer):
+        return 9
--- test_model.py
+++ test_model.py
@@ -2,10 +2,16 @@ import unittest
 
 from model import Model
 
-class TestSingleDeveloperTests(unittest.TestCase):
+class TestSingleDeveloperSingleUnit(unittest.TestCase):
     def test_becomes_master_after_enough_time_passed(self):
         model = Model(units = 1, ask_threshold = 3)
-        model.add_developer([1])
+        model.add_developer('kalle', [1])
         model.step(1000)
-        knowledge = model.get_knowledge(0)
-        assert knowledge = 9
+        knowledge = model.get_knowledge('kalle')
+        assert knowledge == 9
+        
+    def test_knowledge_unchanged_after_no_time(self):
+        model = Model(units = 1, ask_threshold = 3)
+        model.add_developer('kalle', [5])
+        knowledge = model.get_knowledge('kalle')
+        assert knowledge == 5
