c0997da0c4ca76db1844e43be38a5f49e9b5765b
objarni <olof.bjarnason@gmail.com>
2012-02-21 01:00:28 +0100

    Refactored test code.

--- test_model.py
+++ test_model.py
@@ -3,15 +3,16 @@ import unittest
 from model import Model
 
 class TestSingleDeveloperSingleUnit(unittest.TestCase):
+    def setUp(self):
+        self.model = Model(units = 1, ask_threshold = 3, todo = [0]*10000)
+        
     def test_that_developer_becomes_master_after_enough_time(self):
-        model = Model(units = 1, ask_threshold = 3, todo = [0]*10000)
-        model.add_developer('kalle', [1])
-        model.step(1000)
-        knowledge = model.get_knowledge('kalle')
+        self.model.add_developer('kalle', [1])
+        self.model.step(1000)
+        knowledge = self.model.get_knowledge('kalle')
         self.assertEqual([9], knowledge)
         
-    def test_knowledge_unchanged_after_no_time_passed(self):
-        model = Model(units = 1, ask_threshold = 3, todo = [0]*10000)
-        model.add_developer('kalle', [5])
-        knowledge = model.get_knowledge('kalle')
+    def test_that_knowledge_unchanged_after_no_time_passed(self):
+        self.model.add_developer('kalle', [5])
+        knowledge = self.model.get_knowledge('kalle')
         self.assertEqual([5], knowledge)
