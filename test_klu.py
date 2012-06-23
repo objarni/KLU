2d56339acec19cb7d79d6ee595e8c71de9e88abd
toru.ranryu <toru.ranryu@gmail.com>
2012-06-23 17:36:08 +0200

    Unit test for single programmer, two areas, practicing only one

--- test_klu.py
+++ test_klu.py
@@ -10,6 +10,14 @@ class TestForSingleProgrammer(unittest.TestCase):
         programmer = team.member[0]
         programmer_knowledge = programmer.level
         self.assertEqual([9], programmer_knowledge)
+    def test_no_expertise_in_nonpracticed_area(self):
+        team = klu.team(1)
+        project = klu.project(2)
+        project.add_tasks([0]*1000)
+        klu.run(project=project,team=team,level=1)
+        programmer = team.member[0]
+        programmer_knowledge = programmer.level
+        self.assertEqual([9,1], programmer_knowledge)
 
 if __name__ == '__main__':
     unittest.main()
