4189ee2f4301cd5666ba15461476a22bc538010a
Olof Bjarnason <olof.bjarnason@gmail.com>
2012-06-23 12:46:22 +0200

    first unit test added

--- test_klu.py
+++ test_klu.py
@@ -2,5 +2,11 @@ import unittest
 import klu
 
 class TestForSingleProgrammer(unittest.TestCase):
-    def test1(self):
-        self.assertTrue(True)
+    def test_becomes_expert_after_enough_time(self):
+        team = klu.team(1)
+        project = klu.project(1)
+        project.add_random_tasks(1000)
+        klu.run_simulation(project, team)
+        programmer = team.member[0]
+        programmer_knowledge = programmer.level
+        self.assertEqual([9], programmer_knowledge)
