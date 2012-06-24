d4f7754db51040731af33f4f7f2d6d49f3b0d864
Olof Bjarnason <olof.bjarnason@gmail.com>
2012-06-24 12:00:38 +0200

    added desc.py to upload script. make all files runnable on remote server (caused error 500 without chmodding)

--- upload.py
+++ upload.py
@@ -4,7 +4,7 @@
     
     print("Upload script for KLU.")
     print("This script will update the appropriate files on the remote FTP server.")
-    password = getpass("Enter Enter FTP password:")
+    password = getpass("Enter FTP password:")
     if not password:
         return
     ftp = FTP()
@@ -17,16 +17,18 @@
 
     ftp.cwd('olofbjarnason.se/public_html/cgi-bin/0004')
     ftp.sendcmd('TYPE i')
-    files = ['klu.py', 'webklu.py']
+    files = ['klu.py', 'webklu.py', 'desc.py']
     for file in files:
         upload_file(ftp, file)
     ftp.quit()
 
 def upload_file(ftp, file):
     print('Uploading ' + file + ' ...')
-    ftp.delete(file)
+    try: ftp.delete(file)
+    except: pass
     f = open(file, 'r')
     ftp.storlines('STOR ' + file, f)
+    ftp.voidcmd('SITE CHMOD 755 ' + file)
     f.close()
     print('Done.')
 
